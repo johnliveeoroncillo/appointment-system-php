f5c0c0077f7cc35867d5e8dbadc3f49afd19d0c1
John Livee Oroncillo <johnlivee.o@singlife.com>
2023-11-27 14:38:53 +0800

    fix: install node prebuild

--- .platform/hooks/prebuild/install_node_js.sh
+++ .platform/hooks/prebuild/install_node_js.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-if [[ ! "$(node - version)" =~ "v16" ]]; then
-sudo yum install -y gcc-c++ make
-sudo yum remove -y nodejs npm
-sudo rm -rf /var/cache/yum/*
-sudo yum clean all
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
-sudo yum install -y nodejs
+
+# Install  Node  alongside with the paired NPM release
+
+if [[ ! "$(node --version)" =~ "v12" ]]; then
+    sudo yum remove -y nodejs npm
+
+    sudo rm -fr /var/cache/yum/*
+
+    sudo yum clean all
+
+    curl --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -
+
+    sudo yum install nodejs -y
+fi
